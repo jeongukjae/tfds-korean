35c9df4405c824381922cae07b78b852a4f6a694
Jeong Ukjae <JeongUkJae@gmail.com>
2021-04-14 20:43:52 +0900

    add shell script to run all tests

---
+++ run-all-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TEST_CASES=`find tfds_korean -name "*test.py"`
+
+for test_case in $TEST_CASES ; do
+    module_name=`echo $test_case | sed -e 's/\.py$//g' -e 's/\//./g'`
+
+    echo "\n\nRun $test_case ($module_name)\n"
+    python -m $module_name
+done
