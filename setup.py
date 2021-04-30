dfe0ce8c93682e746c7f8243f5ca1974d89fd53b
Jeong Ukjae <JeongUkJae@gmail.com>
2021-04-30 23:46:19 +0900

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ _REQUIRES = [
 
 setup(
     name="tfds-korean",
-    version="0.1.0",
+    version="0.1.1",
     author="Ukjae Jeong",
     author_email="jeongukjae@gmail.com",
     description="A collection of Korean Text Datasets ready to use using Tensorflow-Datasets.",
--- tfds_korean/__init__.py
+++ tfds_korean/__init__.py
@@ -1,2 +1,2 @@
 __author__ = "Ukjae Jeong"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
