420e8ab32a4cd538999b730efae005b6e5947e09
Jeong Ukjae <JeongUkJae@gmail.com>
2021-06-07 01:25:46 +0900

    remove unused comments

--- tfds_korean/korean_wikipedia_corpus/korean_wikipedia_corpus_test.py
+++ tfds_korean/korean_wikipedia_corpus/korean_wikipedia_corpus_test.py
@@ -11,12 +11,6 @@ class KoreanWikipediaCorpusTest(tfds.testing.DatasetBuilderTestCase):
     DATASET_CLASS = korean_wikipedia_corpus.KoreanWikipediaCorpus
     SPLITS = {"train": 3}
 
-    # If you are calling `download/download_and_extract` with a dict, like:
-    #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-    # then the tests needs to provide the fake output paths relative to the
-    # fake data directory
-    # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
-
 
 if __name__ == "__main__":
     tfds.testing.test_main()
